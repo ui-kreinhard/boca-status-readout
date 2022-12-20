7cafa64490f06b513d3cfb6b705fedc4a1b2ab52
Karl-Ludwig Reinhard <reinhard@synyx.de>
2022-12-20 14:41:38 +0100

    Add cmdline

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/ui-kreinhard/boca-status-readout/query"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalln("No ip given")
+	}
+	printerIp := os.Args[1]
+	mode := "count"
+	if len(os.Args) == 3 {
+		mode = os.Args[2]
+	}
+	printerStatus, err := query.FetchStatusWithTimeout(printerIp, 5*time.Second)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	switch mode {
+	case "count":
+		fmt.Println(printerStatus.TicketCount)
+		return
+	case "status":
+		fmt.Println(printerStatus.GetIntStatus())
+	case "json":
+		fmt.Println(printerStatus.ToJson())
+	}
+}
